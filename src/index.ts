bca8ca4529aeeedc81faa39bf18f13b7240fc86a
Vladislav Kamenev <mazahakaforever@ya.ru>
2020-03-05 21:01:35 +0300

    Forward eventy. HTML format.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import "./helpers/env";
 
-import Telegraf, {ContextMessageUpdate, Extra} from "telegraf";
-import { connect } from "mongoose";
+import Telegraf, {ContextMessageUpdate, Extra, Markup} from "telegraf";
+import {connect} from "mongoose";
 
-import { ParticipantModel, MeetingModel } from './models/meeting';
+import {ParticipantModel, MeetingModel} from './models/meeting';
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {
     telegram: {
@@ -25,44 +25,69 @@ const generateTextForMeeting = (meeting) => {
         `${meeting.topic}`,
         '',
         'Записались:',
-        `${meeting.participants.map((participant, idx) => `${idx +1}. <a href="tg://user?id=${participant.tgId}">${participant.displayName}</a>`).join('\n')}`
+        `${meeting.participants.map((participant, idx) => `${idx + 1}. <a href="tg://user?id=${participant.tgId}">${participant.displayName}</a>`).join('\n')}`
     ];
 
     return text.join('\n');
 };
 
-const updateMeetingMessage = (ctx, meeting, action = 'edit') => {
+const updateMeetingMessage = (ctx, meeting, isOwner = true, action = 'edit') => {
     const methods = {
         'edit': ctx.editMessageText,
         'create': ctx.reply
     };
 
-    return methods[action](generateTextForMeeting(meeting), Extra.HTML().markup((m) =>
-        m.inlineKeyboard([
-            m.callbackButton('Я иду', `entry ${meeting.id}`),
-            m.callbackButton('Галка, отмена!', `exit ${meeting.id}`)
-        ])))
+    return methods[action](generateTextForMeeting(meeting), Extra.webPreview(false).HTML().markup((m) =>
+        m.inlineKeyboard(
+            [
+                [
+                    m.callbackButton('Я иду', `entry ${meeting.id}`, isOwner),
+                    m.callbackButton('Галка, отмена!', `exit ${meeting.id}`, isOwner)
+                ],
+                [
+                    m.callbackButton('Обновить данные', `update ${meeting.id}`, !isOwner)
+                ],
+                [
+                    m.switchToChatButton('Переслать', `${meeting.id}`, !isOwner)
+                ]
+            ]
+        )));
 };
 
 bot.hears(/^[!\/]init (.+)$/, async (ctx: ContextMessageUpdate) => {
     const topic = ctx.match[1];
-    const meeting = await MeetingModel.create({ topic, participants: [] });
+    // @ts-ignore
+    const formattedText = Markup.formatHTML(ctx.message.text, ctx.message.entities).slice(6);
+    const meeting = await MeetingModel.create({topic:formattedText, participants: []});
     await meeting.save();
-    return updateMeetingMessage(ctx, meeting, 'create');
+    await updateMeetingMessage(ctx, meeting, true, 'create');
+});
+
+bot.action(/update (.*)/, async (ctx) => {
+    const [_, meetingId] = ctx.match;
+    const meeting = await MeetingModel.findById({_id: meetingId});
+    await meeting.populate('participants').execPopulate();
+    try {
+        await updateMeetingMessage(ctx, meeting, true, 'edit');
+    } catch (ignored) {}
+
+    return ctx.answerCbQuery();
 });
 
 bot.action(/(exit|entry) (.*)/, async (ctx) => {
-    const [ _, action, meetingId ] = ctx.match;
-    const { from:user } = ctx.update.callback_query;
-    let participant = await ParticipantModel.findOne({ tgId: user.id });
+    const [_, action, meetingId] = ctx.match;
+    const {from: user} = ctx.update.callback_query;
+    let participant = await ParticipantModel.findOne({tgId: user.id});
     if (!participant) {
-        participant = await ParticipantModel.create({ tgId: user.id, displayName: user.first_name });
+        participant = await ParticipantModel.create({tgId: user.id, displayName: user.first_name});
         await participant.save();
     }
 
-    const meeting = await MeetingModel.findById({ _id: meetingId});
+    const meeting = await MeetingModel.findById({_id: meetingId});
     await meeting.populate('participants').execPopulate();
-    if (!meeting) { return }
+    if (!meeting) {
+        return
+    }
     if (action === 'entry') {
         if (meeting.participants.some((participant) => participant.tgId === user.id)) {
             return ctx.answerCbQuery(`Вы уже записаны`);
@@ -78,7 +103,7 @@ bot.action(/(exit|entry) (.*)/, async (ctx) => {
         });
     }
     await meeting.save();
-    await updateMeetingMessage(ctx, meeting);
+    await updateMeetingMessage(ctx, meeting, false);
     if (action === 'entry') {
         return ctx.answerCbQuery(`Вы записались`);
     } else {
@@ -86,6 +111,50 @@ bot.action(/(exit|entry) (.*)/, async (ctx) => {
     }
 
 });
+
+bot.on('inline_query', async (ctx) => {
+    const { inlineQuery, answerInlineQuery } = ctx;
+    if (inlineQuery.query && inlineQuery.query.length === 24) {
+        const meeting = await MeetingModel.findById({_id: inlineQuery.query});
+        if (meeting) {
+            return answerInlineQuery(
+                [{
+                    type: 'article',
+                    id: inlineQuery.query,
+                    title: 'Переслать встречу',
+                    description: meeting.topic,
+                    input_message_content: {
+                        message_text: meeting.topic,
+                        parse_mode: 'HTML'
+                    },
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                { text: '⏳ Ждите', callback_data: 'gen' }
+                            ]
+                        ]
+                    }
+                }],
+                {
+                    is_personal: true,
+                    cache_time: 0
+                }
+            )
+        }
+    }
+});
+
+
+bot.on('chosen_inline_result', async (ctx) => {
+    const { chosenInlineResult } = ctx;
+    const { query, inline_message_id } = chosenInlineResult;
+    if (query && inline_message_id) {
+        const meeting = await MeetingModel.findById({_id: query});
+        await meeting.populate('participants').execPopulate();
+        updateMeetingMessage(ctx, meeting, false);
+    }
+});
+
 
 const init = async () => {
     await connect(process.env.DB, {
